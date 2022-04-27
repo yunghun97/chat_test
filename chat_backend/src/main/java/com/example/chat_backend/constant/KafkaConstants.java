59ede152a83c1b3061d582af6260a68c4a81171e
GWON97 <yunghun97@naver.com>
2022-04-27 14:50:40 +0900

    v0.0.2

--- chat_backend/src/main/java/com/example/chat_backend/constant/KafkaConstants.java
+++ chat_backend/src/main/java/com/example/chat_backend/constant/KafkaConstants.java
@@ -3,5 +3,5 @@ package com.example.chat_backend.constant;
 public class KafkaConstants {
     public static final String KAFKA_TOPIC = "chat-test1"; // kafka 토픽 이름
     public static final String GROUP_ID = "foo"; // consumer를 식별할 수 있는 그룹
-    public static final String KAFKA_BROKER = "127.0.0.1:9091"; // kafka 클러스토 호스트:포트
+    public static final String KAFKA_BROKER = "kafka1:9091"; // kafka 클러스토 호스트:포트
 }
