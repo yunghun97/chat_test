67f71d2dec156e9e6c4a4f3a9201a8f298d15565
GWON97 <yunghun97@naver.com>
2022-05-17 15:44:24 +0900

    v0.6.9.2 웹소켓 설정 수정

--- chat_backend/src/main/java/com/example/chat_backend/config/WebSocketConfig.java
+++ chat_backend/src/main/java/com/example/chat_backend/config/WebSocketConfig.java
@@ -1,18 +1,19 @@
 package com.example.chat_backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @EnableWebSocketMessageBroker
 @Configuration
 public class WebSocketConfig  implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("/my-chat").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/my-chat").setAllowedOriginPatterns("*").addInterceptors(new HttpSessionHandshakeInterceptor()).withSockJS();
     }
-
     /*@Override
     public void configureMessageBroker(MessageBrokerRegistry registry){
         registry.setApplicationDestinationPrefixes("/kafka");
